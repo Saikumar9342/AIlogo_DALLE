1e205a7fcd977421b6453492bae1ec92e9b9f91a
pasumarthisaikumar <pasumarthisaikumar2001@gmail.com>
2023-10-05 20:35:56 +0530

    Initial commit

---
+++ client/src/components/ColorPicker.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {SketchPicker} from 'react-color'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
+import { color } from 'framer-motion'
+
+const ColorPicker = () => {
+
+  const snap = useSnapshot(state)
+  return (
+    <div className='absolute left-full ml-3'>
+       <SketchPicker
+         color = {snap.color}
+         disableAlpha
+         onChange={(color)=> state.color = color.hex}
+         >
+       </SketchPicker>
+    </div>
+  )
+}
+
+export default ColorPicker
---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+import CustomButton from "./Custombutton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export {CustomButton,
+        AIPicker,
+        ColorPicker,
+        FilePicker,
+        Tab 
+}
